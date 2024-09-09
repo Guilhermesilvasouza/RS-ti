e8c8bac8073869b845243cff3034dd7c404d35f1
Guilherme Souza <guilhermesouza1812@gmail.com>
2024-09-09 17:42:44 -0300

    exercicio

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("Hello, World")
+let numero1 = 9;
+let numero2 = 2;
+let numero3 = 10;
+let soma = numero1 + numero2 + numero3;
+let media = soma/3;
+console.log("A soma dos números é:" + media);
